b05017ccf31e1c9bc456a60fb99aa25f06750304
Alexander Kozhurkin <kauker01@yandex.ru>
2020-01-20 19:25:19 +0300

    initial draw features implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
+import Canvas from './Canvas';
+
 function App() {
+  const [jsonData, setJsonData] = React.useState('');
+
+  const [width] = useWindowSize();
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setJsonData(value);
+  }
+
   return (
     <div className="app">
-      
+      <Canvas data={jsonData} width={Math.min(Math.max(width, 500), 1500) - 30}/>
+      <textarea
+        rows="30"
+        value={jsonData}
+        onChange={handleChange}
+      >
+      </textarea>
     </div>
   );
 }
 
 export default App;
+
+function useWindowSize() {
+  const [size, setSize] = React.useState([0, 0]);
+  React.useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
---
+++ src/Canvas.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const RATIO = 5 / 2;
+
+class Canvas extends React.Component {
+    componentDidMount() {
+        const canvas = this.refs.canvas
+        this.ctx = canvas.getContext("2d");
+    }
+
+    componentDidUpdate(prevProps) {
+        const { data, width } = this.props;
+        if(prevProps.data !== data || width !==prevProps.width) {  
+            this.clearCanvas();
+            const height = width / RATIO;
+            const center = [width / 2, height / 2];
+            this.ctx.translate(center[0], center[1]);
+            this.draw();
+        }
+    }
+
+    clearCanvas = () => {
+        const canvas = this.refs.canvas;
+
+        // Use the identity matrix while clearing the canvas
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    draw = () => {
+        const { data, width } = this.props;
+        
+        try {
+            const json = JSON.parse(data);
+            // only use nodes with specific key
+            const dataToRender = json.filter(item => item["Prefab"] = "HandLineRendererVariant")
+            
+            const SCALE = width / 2;
+
+            dataToRender.forEach(item => {
+                const strokeArr = item["Stroke"];
+                if (!item["Stroke"]) return
+                const linePoints = [],
+                    chunkSize = 3;
+                for (let i = 0, len = strokeArr.length; i < len; i += chunkSize)
+                    linePoints.push(strokeArr.slice(i, i + chunkSize));
+
+                this.ctx.beginPath();
+                const startPoint = linePoints[0]
+                this.ctx.moveTo(startPoint[0] * SCALE, -startPoint[1] * SCALE);
+                for (let i = 1; i < linePoints.length; i++) {
+                    const point = linePoints[i];
+                    if (item["PointMask"][i] === 1) {
+                        this.ctx.lineTo(point[0] * SCALE, - point[1] * SCALE);
+                    } else {
+                        this.ctx.moveTo(point[0] * SCALE, - point[1] * SCALE)
+                    }
+                    
+                }
+
+                this.ctx.stroke();
+            })
+        } catch (e) {
+            console.error(e);
+        }
+        
+    }
+
+    render() {
+        const { width } = this.props;
+        return(
+            <div>
+            <canvas ref="canvas" width={width} height={width / RATIO} />
+            </div>
+        )
+    }
+  }
+  export default Canvas
