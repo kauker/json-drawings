99abb39d89d2374c4e5a00d271516aff5e6c5e41
Alexander Kozhurkin <kauker01@yandex.ru>
2020-01-20 19:38:50 +0300

    Updates

---
+++ precache-manifest.92bea210b4c82d5e9a99b3e99e142d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f66acd5c450f1ddef192e27e1c40e1",
+    "url": "/json-drawings/index.html"
+  },
+  {
+    "revision": "37958c1ed88d0c1607b2",
+    "url": "/json-drawings/static/css/main.4b81b196.chunk.css"
+  },
+  {
+    "revision": "bde20f82b30cdcc36509",
+    "url": "/json-drawings/static/js/2.51ddb2bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/json-drawings/static/js/2.51ddb2bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37958c1ed88d0c1607b2",
+    "url": "/json-drawings/static/js/main.478b7473.chunk.js"
+  },
+  {
+    "revision": "f8e7997fb6f7052180e0",
+    "url": "/json-drawings/static/js/runtime-main.5707b0f4.js"
+  }
+]);
